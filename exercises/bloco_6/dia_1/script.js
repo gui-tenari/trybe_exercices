81e4762cf6881be8bdafcd6e28947d969c2bfaf0
Tenari <guilherme.tenarie@gmail.com>
2021-06-10 10:45:50 -0300

    adiciona estados do Brasil e termina exercicio 1

---
+++ exercises/bloco_6/dia_1/script.js
@@ -0,0 +1,12 @@
+import states from './states_of_Brasil'
+const statesList = document.querySelector('#user-state')
+
+console.log(states)
+
+function createOptions (states) {
+    for (let index = 0; index < states.length; index += 1) {
+        const state = document.createElement('option');
+        option
+        
+    }
+}
